6a2101e7be3fbb5bcb1b4e6f0d0194b332627a48
Vonrie Viscayno <viscaynovonrie@gmail.com>
2021-04-20 16:01:14 +0800

    add forecast description, fix about text typo, and change about portrait

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request")
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=8e09a48a4b40c806ed7671ef4ff0a8ee&query='+ latitude +',' + longitude +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=8e09a48a4b40c806ed7671ef4ff0a8ee&query='+ latitude +',' + longitude +'&units=m'
 
     request({url, json: true}, (error, {body}) => {
             if (error){
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback) => {
             } else if (body.error) {
                 callback(response.body.error.info, undefined)
             } else {
-                callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.')
+                callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +
+                 ' degrees out. It feels like ' + body.current.feelslike + ' degrees. ' +
+                 'Humidity is ' + body.current.humidity + ' percent.')
             }
         })
 
